b6776e410213470eeae53968d4c55bb61523bb9e
Pankoj-Dakua <pankoj6@gmail.com>
2025-10-04 09:49:33 +0600

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,103 @@
+// "use strict";
+// alert("Hi hallow");
+
+// //Object Class
+// const mobileBrand = {
+//     brand: 'Samsung',
+//     model: 'Sz 400 su',
+//     processor: 'intel',
+//     camera: ['120MP', '12MP', '36MP'],
+//     'has Zoom Camera': true,
+
+// }
+
+// console.log(mobileBrand['has Zoom Camera']);
+// console.log(mobileBrand.processor);
+
+// document.getElementById("demo").innerHTML = "The a camera processor is:  " + mobileBrand['has Zoom Camera'];
+
+
+// const person ={};
+
+// person.name = "Jone";
+// person.age = "35";
+
+// console.log(person.name + " " + person.age);
+
+// // Array Class
+
+// const language = ["CSS", "Javascript", "PHP", "HTML", true, 50];
+
+// console.log(language);
+
+// console.log(language[2]);
+// language[1] = "Phython";
+// console.log(language);
+// language[4] = "False";
+// console.log(language);
+// console.log(language.toString());
+// console.log(language.length);
+// // document.getElementById("demo").innerHTML = language[2] = "PHPOGO";
+// let lang = language.at(3);
+// document.getElementById("demo").innerHTML = lang; 
+// let langu = language[4];
+// document.getElementById("demo").innerHTML = langu;
+// document.getElementById("demo").innerHTML = language.join(" * ");
+
+// document.getElementById("demo1").innerHTML = language;
+// // language.pop();
+// document.getElementById("demo2").innerHTML = language;
+
+// language.pop();
+// console.log(language);
+// language.push("banana");
+// document.getElementById("demo2").innerHTML = language;
+// console.log(language);
+// language.shift();
+// document.getElementById("demo1").innerHTML = language;
+// language.unshift(50);
+// console.log(language);
+// delete language[0];
+// console.log(language);
+// document.getElementById("demo1").innerHTML = "The first item is : ";
+
+
+
+
+// Event class
+function subscrib() 
+    {document.getElementById("button").innerHTML = "Subscribed";
+
+}
+
+function subscrib() {
+    document.getElementById("button").innerHTML = "Subscribed";
+    document.getElementById("button").style.backgroundColor = "#262626";
+
+}
+function link() {
+    document.getElementById("button2").innerHTML = '<i class="fa-solid fa-thumbs-up"></i>Subscribed';
+
+}
+// ---------------------------------------------------
+// Start jascript for navbar_5.html
+
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display='flex'
+}
+function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display='none'
+}
+
+// End jascript for navbar_5.html
+
+
+
+
+
+
+
+
+
